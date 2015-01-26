09065d271068db7df416b4df07bca3b5fea529b0
Andrew Branch <andrewbranch@mail.com>
2015-01-26 16:13:04 -0600

    Add loading state

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('categories', { path: '/' });
+  this.route("loading");
 });
 
 export default Router;
---
+++ frontend/app/views/loading.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+
+  didInsertElement: function () {
+    $('#container').addClass('loading');
+  },
+
+  willDestroyElement: function () {
+    $('#container').removeClass('loading');
+  }
+
+});
