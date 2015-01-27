10c4ae8d43d8d90a3e7e9379b645fac66186562e
Andrew Branch <andrewbranch@mail.com>
2015-01-27 12:21:04 -0600

    Update with mobile optimizations

--- assets/x-todo.js
+++ assets/x-todo.js
@@ -209,6 +209,16 @@ define("x-todo/components/date-time-field",
 
       classNames: ["date-time-field", "ui", "input"],
 
+      isMobile: (function () {
+        var check = false;
+        (function (a) {
+          if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) {
+            check = true;
+          }
+        })(navigator.userAgent || navigator.vendor || window.opera);
+        return check;
+      }).property(),
+
       hour: (function () {
         return this.get("date").hour();
       }).property("date"),
@@ -253,6 +263,45 @@ define("x-todo/components/date-time-field",
         }
       }
 
+    });
+  });
+define("x-todo/components/native-date", 
+  ["ember","exports"],
+  function(__dependency1__, __exports__) {
+    "use strict";
+    /* global moment */
+
+    var Ember = __dependency1__["default"];
+
+    __exports__["default"] = Ember.TextField.extend({
+
+      didInsertElement: function () {
+        this.$().focus();
+      },
+
+      textToDateTransform: (function (key, value) {
+        var date;
+        if (arguments.length === 2) {
+          if (value && /\d{4}-\d{2}-\d{2}/.test(value)) {
+            var parts = value.split("-");
+            date = new Date();
+            date.setYear(parts[0]);
+            date.setMonth(parts[1] - 1);
+            date.setDate(parts[2]);
+
+            this.set("date", moment(date));
+          } else {
+            this.set("date", null);
+          }
+        } else if (!value && this.get("date")) {
+          return this.get("date").format("YYYY-MM-DD");
+        } else {
+          return value;
+        }
+      }).property(),
+
+      placeholder: "yyyy-mm-dd",
+      valueBinding: "textToDateTransform"
     });
   });
 define("x-todo/components/replacing-field", 
@@ -1301,6 +1350,78 @@ define("x-todo/templates/components/date-time-field",
   function(__exports__) {
     "use strict";
     __exports__["default"] = Ember.HTMLBars.template((function() {
+      var child0 = (function() {
+        return {
+          isHTMLBars: true,
+          blockParams: 0,
+          cachedFragment: null,
+          hasRendered: false,
+          build: function build(dom) {
+            var el0 = dom.createDocumentFragment();
+            var el1 = dom.createTextNode("  ");
+            dom.appendChild(el0, el1);
+            var el1 = dom.createTextNode("\n");
+            dom.appendChild(el0, el1);
+            return el0;
+          },
+          render: function render(context, env, contextualElement) {
+            var dom = env.dom;
+            var hooks = env.hooks, inline = hooks.inline;
+            dom.detectNamespace(contextualElement);
+            var fragment;
+            if (this.cachedFragment === null) {
+              fragment = this.build(dom);
+              if (this.hasRendered) {
+                this.cachedFragment = fragment;
+              } else {
+                this.hasRendered = true;
+              }
+            }
+            if (this.cachedFragment) {
+              fragment = dom.cloneNode(this.cachedFragment, true);
+            }
+            var morph0 = dom.createMorphAt(fragment,0,1,contextualElement);
+            inline(env, morph0, context, "native-date", [], {"type": "date", "dateBinding": "date"});
+            return fragment;
+          }
+        };
+      }());
+      var child1 = (function() {
+        return {
+          isHTMLBars: true,
+          blockParams: 0,
+          cachedFragment: null,
+          hasRendered: false,
+          build: function build(dom) {
+            var el0 = dom.createDocumentFragment();
+            var el1 = dom.createTextNode("  ");
+            dom.appendChild(el0, el1);
+            var el1 = dom.createTextNode("\n");
+            dom.appendChild(el0, el1);
+            return el0;
+          },
+          render: function render(context, env, contextualElement) {
+            var dom = env.dom;
+            var hooks = env.hooks, get = hooks.get, inline = hooks.inline;
+            dom.detectNamespace(contextualElement);
+            var fragment;
+            if (this.cachedFragment === null) {
+              fragment = this.build(dom);
+              if (this.hasRendered) {
+                this.cachedFragment = fragment;
+              } else {
+                this.hasRendered = true;
+              }
+            }
+            if (this.cachedFragment) {
+              fragment = dom.cloneNode(this.cachedFragment, true);
+            }
+            var morph0 = dom.createMorphAt(fragment,0,1,contextualElement);
+            inline(env, morph0, context, "date-field", [], {"class": "date", "date": get(env, context, "date")});
+            return fragment;
+          }
+        };
+      }());
       return {
         isHTMLBars: true,
         blockParams: 0,
@@ -1310,7 +1431,7 @@ define("x-todo/templates/components/date-time-field",
           var el0 = dom.createDocumentFragment();
           var el1 = dom.createTextNode("");
           dom.appendChild(el0, el1);
-          var el1 = dom.createTextNode("\n");
+          var el1 = dom.createTextNode("");
           dom.appendChild(el0, el1);
           var el1 = dom.createTextNode(":");
           dom.appendChild(el0, el1);
@@ -1322,7 +1443,55 @@ define("x-todo/templates/components/date-time-field",
         },
         render: function render(context, env, contextualElement) {
           var dom = env.dom;
-          var hooks = env.hooks, get = hooks.get, inline = hooks.inline;
+          var hooks = env.hooks, get = hooks.get, block = hooks.block, inline = hooks.inline;
+          dom.detectNamespace(contextualElement);
+          var fragment;
+          if (this.cachedFragment === null) {
+            fragment = this.build(dom);
+            if (this.hasRendered) {
+              this.cachedFragment = fragment;
+            } else {
+              this.hasRendered = true;
+            }
+          }
+          if (this.cachedFragment) {
+            fragment = dom.cloneNode(this.cachedFragment, true);
+          }
+          if (this.cachedFragment) { dom.repairClonedNode(fragment,[0,1]); }
+          var morph0 = dom.createMorphAt(fragment,0,1,contextualElement);
+          var morph1 = dom.createMorphAt(fragment,1,2,contextualElement);
+          var morph2 = dom.createMorphAt(fragment,2,3,contextualElement);
+          var morph3 = dom.createMorphAt(fragment,3,4,contextualElement);
+          block(env, morph0, context, "if", [get(env, context, "isMobile")], {}, child0, child1);
+          inline(env, morph1, context, "replacing-field", [], {"class": "hour", "value": get(env, context, "formattedHour"), "pattern": "[0-9]*", "advance": "advance", "advanceCharacters": "SHIFTº", "validationPattern": "^[1-9]$|^1[0-2]$"});
+          inline(env, morph2, context, "replacing-field", [], {"class": "minute", "value": get(env, context, "minute"), "advance": "advance", "advanceCharacters": " ", "pattern": "[0-9]*", "validationPattern": "^[0-5]$|^[0-5][0-9]$"});
+          inline(env, morph3, context, "replacing-field", [], {"class": "meridiem", "value": get(env, context, "meridiem"), "autocompleteOptions": "AM,PM"});
+          return fragment;
+        }
+      };
+    }()));
+  });
+define("x-todo/templates/components/native-date", 
+  ["exports"],
+  function(__exports__) {
+    "use strict";
+    __exports__["default"] = Ember.HTMLBars.template((function() {
+      return {
+        isHTMLBars: true,
+        blockParams: 0,
+        cachedFragment: null,
+        hasRendered: false,
+        build: function build(dom) {
+          var el0 = dom.createDocumentFragment();
+          var el1 = dom.createTextNode("");
+          dom.appendChild(el0, el1);
+          var el1 = dom.createTextNode("\n");
+          dom.appendChild(el0, el1);
+          return el0;
+        },
+        render: function render(context, env, contextualElement) {
+          var dom = env.dom;
+          var hooks = env.hooks, content = hooks.content;
           dom.detectNamespace(contextualElement);
           var fragment;
           if (this.cachedFragment === null) {
@@ -1338,13 +1507,7 @@ define("x-todo/templates/components/date-time-field",
           }
           if (this.cachedFragment) { dom.repairClonedNode(fragment,[0]); }
           var morph0 = dom.createMorphAt(fragment,0,1,contextualElement);
-          var morph1 = dom.createMorphAt(fragment,1,2,contextualElement);
-          var morph2 = dom.createMorphAt(fragment,2,3,contextualElement);
-          var morph3 = dom.createMorphAt(fragment,3,4,contextualElement);
-          inline(env, morph0, context, "date-field", [], {"class": "date", "date": get(env, context, "date")});
-          inline(env, morph1, context, "replacing-field", [], {"class": "hour", "value": get(env, context, "formattedHour"), "pattern": "[0-9]*", "advance": "advance", "advanceCharacters": "SHIFTº", "validationPattern": "^[1-9]$|^1[0-2]$"});
-          inline(env, morph2, context, "replacing-field", [], {"class": "minute", "value": get(env, context, "minute"), "advance": "advance", "advanceCharacters": " ", "pattern": "[0-9]*", "validationPattern": "^[0-5]$|^[0-5][0-9]$"});
-          inline(env, morph3, context, "replacing-field", [], {"class": "meridiem", "value": get(env, context, "meridiem"), "autocompleteOptions": "AM,PM"});
+          content(env, morph0, context, "yield");
           return fragment;
         }
       };
@@ -1777,6 +1940,15 @@ define("x-todo/tests/components/date-time-field.jshint",
       ok(true, 'components/date-time-field.js should pass jshint.'); 
     });
   });
+define("x-todo/tests/components/native-date.jshint", 
+  [],
+  function() {
+    "use strict";
+    module('JSHint - components');
+    test('components/native-date.js should pass jshint', function() { 
+      ok(true, 'components/native-date.js should pass jshint.'); 
+    });
+  });
 define("x-todo/tests/components/replacing-field.jshint", 
   [],
   function() {
@@ -2075,6 +2247,29 @@ define("x-todo/tests/unit/components/date-time-field-test",
     var test = __dependency1__.test;
 
     moduleForComponent("date-time-field", "DateTimeFieldComponent", {});
+
+    test("it renders", function () {
+      expect(2);
+
+      // creates the component instance
+      var component = this.subject();
+      equal(component._state, "preRender");
+
+      // appends the component to the page
+      this.append();
+      equal(component._state, "inDOM");
+    });
+    // specify the other units that are required for this test
+    // needs: ['component:foo', 'helper:bar']
+  });
+define("x-todo/tests/unit/components/native-date-test", 
+  ["ember-qunit"],
+  function(__dependency1__) {
+    "use strict";
+    var moduleForComponent = __dependency1__.moduleForComponent;
+    var test = __dependency1__.test;
+
+    moduleForComponent("native-date", "NativeDateComponent", {});
 
     test("it renders", function () {
       expect(2);
@@ -2492,6 +2687,15 @@ define("x-todo/tests/x-todo/tests/unit/components/date-time-field-test.jshint",
     module('JSHint - x-todo/tests/unit/components');
     test('x-todo/tests/unit/components/date-time-field-test.js should pass jshint', function() { 
       ok(true, 'x-todo/tests/unit/components/date-time-field-test.js should pass jshint.'); 
+    });
+  });
+define("x-todo/tests/x-todo/tests/unit/components/native-date-test.jshint", 
+  [],
+  function() {
+    "use strict";
+    module('JSHint - x-todo/tests/unit/components');
+    test('x-todo/tests/unit/components/native-date-test.js should pass jshint', function() { 
+      ok(true, 'x-todo/tests/unit/components/native-date-test.js should pass jshint.'); 
     });
   });
 define("x-todo/tests/x-todo/tests/unit/components/replacing-field-test.jshint", 
